764efe466290b74d59043b4d3daad35a84f6f5ae
julio <julio.sanic.gt.256@gmail.com>
2024-05-02 17:18:32 -0600

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./db/connectMongoDB')
 const cookieParser = require('cookie-parser')
-const cors = require('cors')
 const {
     app,
     server
 } = require('./socket/socket')
 
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
+
 
 // Middleware
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true}))
 app.use(express.json()) // to parse the incoming requests with JSON payloads (from req.body) 
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send('working...')
-})
-
 app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/messages', require('./routes/messageRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'))
+})
 
 server.listen(PORT, () => {
     connectDB()
